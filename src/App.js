00a6b072134d0a0ba23efd68ac71252c1057b009
dinrol <rakov0@mail.ru>
2020-10-28 13:59:16 +0300

    update

--- src/App.js
+++ src/App.js
@@ -5,9 +5,18 @@ import "./css/style.css";
 function App() {
 
   let packageList = [
-    { title: 'с фуагра', numberServings: 10, numberMice: null, weight: '0,5', },
-    { title: 'с рыбой', numberServings: 40, numberMice: 2, weight: '2', },
-    { title: 'с курой', numberServings: 100, numberMice: 5, weight: '5', },
+    {
+      id: 0, title: 'с фуагра', numberServings: 10, numberMice: null,
+      weight: '0,5', noteSelected: "Печень утки разварная с артишоками.", noteDisabled: 'Печалька, с фуа-гра закончился.'
+    },
+    {
+      id: 1, title: 'с рыбой', numberServings: 40, numberMice: 2,
+      weight: '2', noteSelected: "Головы щучьи с чесноком да свежайшая сёмгушка.", noteDisabled: 'Печалька, с рыбой закончился.'
+    },
+    {
+      id: 2, title: 'с курой', numberServings: 100, numberMice: 5,
+      weight: '5', noteSelected: "Филе из цыплят с трюфелями в бульоне.", noteDisabled: 'Печалька, с курой закончился.'
+    },
   ]
   return (
     <div className="wrapper">
--- src/components/Package.jsx
+++ src/components/Package.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react'
 
 export default function Package({ item }) {
+
    let [presentMice, setPresentMice] = useState()
    let [packageSelected, setPackageSelected] = useState(0)
-   let [note, setNote] = useState('0')
-
 
    useEffect(() => {
       if (item.numberMice == null) {
@@ -15,19 +14,37 @@ export default function Package({ item }) {
          setPresentMice('мышей в подарок заказчик доволен')
       }
    }, item.numberMice)
+
+   let mouseLeaveFun = () => {
+      if (document.querySelectorAll(".column.active")) {
+         let x = document.querySelectorAll(".column.active")
+         x.forEach(i => i.classList.add('activeHover'))
+      }
+   }
    let change_state = () => {
       setPackageSelected((packageSelected + 1) % 3)
    }
-   let stateClass = '';
+
+   let stateClass = ''
+   let note = ''
+
    switch (packageSelected) {
       case 1:
          stateClass = 'active'
+         note = item.noteSelected
          break
       case 2:
          stateClass = 'disabled'
+         note = item.noteDisabled
          break
-
    }
+
+   useEffect(() => {
+      if (packageSelected == 1) {
+         document.querySelectorAll(".column.active").forEach(i => i.addEventListener("mouseleave", mouseLeaveFun))
+      }
+   }, [packageSelected]);
+
    return (
       <div className="column-block">
          <div onClick={change_state} className={"column " + stateClass}>
@@ -36,8 +53,11 @@ export default function Package({ item }) {
             <div className="column__promo"><div><span>{item.number_servings}</span> порций</div>  <span>{item.numberMice}</span> {presentMice}</div>
             <div className="column__weight"><p>{item.weight}</p> кг</div>
          </div>
-         <div className="column__note">
-            Чего сидишь? Порадуй котэ, <a href="#">купи.</a>
+         <div style={{ display: packageSelected != 0 ? "none" : "block" }} className="column__note ">
+            Чего сидишь? Порадуй котэ, <a onClick={change_state} href="#">купи.</a>
+         </div>
+         <div className={"column__note " + stateClass}>
+            {note}
          </div>
       </div>
    )
