984842dd5d477c9fa8c0631f7462a0a1f1e9474c
dinrol <rakov0@mail.ru>
2020-10-28 17:05:40 +0300

    Updates

---
+++ precache-manifest.2c0ef0f1a6d29e2bc5e6e9ee40058cbf.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc1ca9364d3e318ce6f032f9c4c7a16c",
+    "url": "/react-task-cat/index.html"
+  },
+  {
+    "revision": "f33cd5554331d93f327d",
+    "url": "/react-task-cat/static/css/main.35f5e6da.chunk.css"
+  },
+  {
+    "revision": "94b3eb49f7eb2415996d",
+    "url": "/react-task-cat/static/js/2.bb17b057.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/react-task-cat/static/js/2.bb17b057.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f33cd5554331d93f327d",
+    "url": "/react-task-cat/static/js/main.7a113dd8.chunk.js"
+  },
+  {
+    "revision": "cedd4ace212771ebe673",
+    "url": "/react-task-cat/static/js/runtime-main.10f0c276.js"
+  },
+  {
+    "revision": "8e412cf5e5954e6134e979e7f9cb9e17",
+    "url": "/react-task-cat/static/media/Pattern.8e412cf5.png"
+  },
+  {
+    "revision": "d63d21344cd1d37227c94334e731a864",
+    "url": "/react-task-cat/static/media/cat.d63d2134.png"
+  },
+  {
+    "revision": "536d58ec5403827583d8fd6eb2e57d19",
+    "url": "/react-task-cat/static/media/trebuc.536d58ec.svg"
+  },
+  {
+    "revision": "bbec02d9e3340068675995ab857ec6ea",
+    "url": "/react-task-cat/static/media/trebuc.bbec02d9.woff"
+  },
+  {
+    "revision": "ea3fdfa648b7ec0e4183621443f549e3",
+    "url": "/react-task-cat/static/media/trebuc.ea3fdfa6.ttf"
+  },
+  {
+    "revision": "f68bcb3ca5482544b6827d339bf95dd5",
+    "url": "/react-task-cat/static/media/trebuc.f68bcb3c.eot"
+  },
+  {
+    "revision": "2e5a9d07d1d34c5bd60d4dbaf52639c0",
+    "url": "/react-task-cat/static/media/trebucbd.2e5a9d07.woff"
+  },
+  {
+    "revision": "40ab859220ec60c65d9e71f5bff71ab2",
+    "url": "/react-task-cat/static/media/trebucbd.40ab8592.svg"
+  },
+  {
+    "revision": "b98347bbfab98aa20f393381bb6d2d49",
+    "url": "/react-task-cat/static/media/trebucbd.b98347bb.ttf"
+  },
+  {
+    "revision": "d80e88ed9cd4c9705b222ed744c9941a",
+    "url": "/react-task-cat/static/media/trebucbd.d80e88ed.eot"
+  }
+]);
